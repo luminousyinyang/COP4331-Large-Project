bb1c2d68c5d476394feb1103fe58d2852ad87418
Loren P <160626485+lore4prez@users.noreply.github.com>
2025-04-16 23:25:46 -0400

    Add more hover and transition styles

---
+++ frontend/src/components/mini-comp/productItem.tsx
@@ -0,0 +1,10 @@
+const Item = ({ className, ...props }: React.ComponentProps<"div">) => {
+    
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Item;
--- frontend/src/pages/landing.tsx
+++ frontend/src/pages/landing.tsx
@@ -5,28 +5,21 @@ import { ItemContainer } from "@/components/items-container";
 import { ProfileBar } from "@/components/profileBar";
 
 function LandingPage() {
-  // return (
-  //   <div  className="w-full min-h-screen grid bg-[var(--bg-sandpaper)]"
-  //         style={{ 
-  //           gridTemplateColumns: "2.5fr 1fr",
-  //           gridTemplateRows: "1fr 5fr",
-  //           gridTemplateAreas: `
-  //             "optionsBar profile"
-  //             "itemContainer profile"
-  //         ` }}>
-  //       <OptionsBar style={{ gridArea: "optionsBar" }} className="mt-12 ml-20"/>
-  //       <ItemContainer style={{ gridArea: "itemContainer" }} className="ml-20"/>
-  //       <ProfileBar style={{ gridArea: "profile" }}/>
-  //   </div>
-  // )
-
+  
   return (
     <div  className="flex flex-row justify-between overflow-visible w-full min-h-screen bg-[var(--bg-sandpaper)]" >
         <div className="flex flex-col gap-5">
           <OptionsBar  className="pt-10 ml-25"/>
           <ItemContainer  className="ml-25"/>
         </div>
-        <ProfileBar className="h-screen sticky top-0 right-0"/>
+        <ProfileBar className="h-screen sticky top-0 right-0" 
+          firstName={"First"} 
+          lastName={"Last"}
+          about={"Lorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum is simply dummy text of the printing and typesetting industryLorem Ipsum is simply dummy text of the printing and typesetting industry ..."}
+          instaLink={""} 
+          spotifyLink={""} 
+          twitterLink={""}
+        />
     </div>
   )
 }
